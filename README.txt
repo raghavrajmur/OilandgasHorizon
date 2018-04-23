becdd26c1f4b906e0510a8ea71e49b1aa281046e
raghavrajmur <rajeshraghavanmail@gmail.com>
2018-04-23 10:53:10 +0200

    App

---
+++ README.txt
@@ -0,0 +1,15 @@
+This project is created using Visual Studio 2015. 
+
+Prerequisites
+1. Visual Studio 2015 IDE
+
+
+Instructions
+1. Unzip the file to your local repository (eg: "C:\Users\raj\Documents\WpfApplication")
+2. Open the WpfApplication4.sln file in Visual Studio 2015
+3. After verifying that the project is completely loaded in Solution explorer run the Project or type the F5 key on keyboard
+4. Browse the depthValuesCSV file
+5. Choose a Unit using the radio button provided (Try choosing different units each time eg. "cubic Feet" or "cubic meter" or "barrels"
+6. Click on "Rechnen" button to calculate the volume
+7. Observe the calculated value in the "Volumen wert" text box.
+8. Click on "Beenden" to exit the application
---
+++ WpfApplication4/RechnVolume.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication
+{
+    class RechnVolume
+    {
+
+        private const double CubicFeet2CubicMeter = 0.0283168;
+        private const double CubicFeet2Barrel = 0.237476809;
+
+        private List<long> tops;
+        private RechnParameters rpm;
+
+        public RechnVolume(List<long> tops, ref RechnParameters rechnParam)
+        {
+            this.tops = tops;
+            this.rpm = rechnParam;
+        }
+
+        public double ReVolume()
+        {
+            double vol = 0.0;
+            double sum = 0.0;
+            double bot = 0.0;
+            
+            
+
+            foreach (long top in tops)
+            {
+                bot = top + rpm.BASEDELTA;
+                sum += top >= rpm.FLUIDCONTACT ? 0.0 : bot > rpm.FLUIDCONTACT ? rpm.FLUIDCONTACT - top : bot - top;
+            }
+
+            switch (rpm.UNIT)
+            {
+                case VolumeUnit.CubicFeet:
+                    vol = rpm.CELLAREA * sum;
+                    break;
+                case VolumeUnit.CubicMeter:
+                    vol = rpm.CELLAREA * sum * CubicFeet2CubicMeter;
+                    break;
+                case VolumeUnit.Barrel:
+                    vol = rpm.CELLAREA * sum * CubicFeet2Barrel;
+                    break;
+                default:
+                    break;
+            }
+
+            return vol;
+        }
+    }
+}
